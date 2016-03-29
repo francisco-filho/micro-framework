5c2ffa4a0dafa6b309546a47d468f83b48f8a80a
francisco-filho <francisco@capimgrosso.com>
2016-03-28 22:54:45 -0300

    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import database.DBConnection;
+import database.DB;
 import database.Row;
 import database.RowList;
 import server.App;
 import server.middleware.AutenticadorOpenAM;
 import server.middleware.Logger;
-
-import java.sql.SQLException;
 
 /**
  * Created by f3445038 on 21/03/16.
@@ -14,27 +12,26 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         App app = new App((config) -> {
-            config.setServeStatic(false);
+            config.setServeStatic(true);
             config.useConnectionPool(true);
         });
         //app.use(new AutenticadorOpenAM());
         app.use(new Logger());
 
         app.get("/api/teste/:id", (req, res) -> {
-            DBConnection db = app.getDb("production");
+            DB db = app.db("production");
             Row row = db.first("SELECT DISTINCT id, municipio, uf FROM municipios WHERE id = ?", req.params.getInt("id"));
             res.json(row);
         });
 
         app.get("/api/teste/:name/:id", (req, res) -> {
-            DBConnection db = app.getDb("production");
+            DB db = app.db("production");
             /*
             List<FileItem> files = (List<FileItem>)req.params.remove("files");
             for (FileItem f : files) {
                 System.out.println(f);
             }*/
             RowList row = db.tx((portal) -> {
-
                 return portal.list("SELECT DISTINCT municipio, uf FROM municipios WHERE id = ?", req.params.getInt("id"));
             });
             res.status(200).json(row);
